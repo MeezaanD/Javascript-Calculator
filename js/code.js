e2f0847e35a75ce92de28bfa0a26c077991a1e97
Meezaan Davids <meezaandavids365@gamil.com>
2022-11-26 11:51:13 +0200

    update

--- js/code.js
+++ js/code.js
@@ -1,32 +1,49 @@
-let screen = document.getElementById('screen');
-buttons = document.querySelectorAll('button');
+let button = document.querySelectorAll('button');
 let screenValue = '';
-let decimal = document.getElementById('decimal')
-for (item of buttons) {
-    item.addEventListener('click', (e) => {
-        buttonText = e.target.innerText;
-        console.log('Button text is ', buttonText);
-        if (buttonText == 'X') {
-            buttonText = '*';
-            screenValue += buttonText;
-            screen.value = screenValue;
-            
-        }
-        else if (buttonText == 'AC') {
-            screenValue = "";
-            screen.value = screenValue;
-        }
-        else if (buttonText == '=') {
-            screen.value = eval(screenValue);
-        }
-        else {
-            screenValue += buttonText;
-            screen.value = screenValue;
-        }
-        // if (decimal === true) {
-        //     buttonText = '.';
-        //     decimal = false;
-        // }
-    }) 
+
+
+let allowDecimal = true;
+function displayDecimal(value) {
+    if (allowDecimal) {
+        screen.value += value
+        allowDecimal = false
+    }
 }
 
+let screen = document.getElementById('screen');
+function display(arg){
+    screen.value += arg;
+}
+
+function displayOp(arg){
+    
+    screen.value += arg;
+    allowDecimal = true
+}
+
+const equalBtn = document.querySelector('.equal');
+equalBtn.addEventListener ('click', ()=>{
+    if (screen !== ''){
+        screen.value = eval(screen.value);
+
+        if (screen.value.includes('.')){
+            allowDecimal = false
+        }else allowDecimal = true
+    }
+})
+
+const clearBtn = document.getElementById('clear')
+clearBtn.addEventListener('click', ()=>{
+    if (screen !==''){
+        screen.value = "";
+        allowDecimal= true;
+    }
+})
+
+
+
+
+
+
+
+ 
